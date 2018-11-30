a4948d4b2607fc5b184a089a5caf6f155fc94dbc
Unknown <belleyjeanphilippe@gmail.com>
2018-11-29 18:27:12 -0600

    Basic design

--- src/App.js
+++ src/App.js
@@ -3,23 +3,50 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    var html = document.getElementById("html");
+    var css = document.getElementById("css");
+    var js = document.getElementById("js");
+    var code = document.getElementById("code").contentWindow.document;
+
+    document.body.onkeyup = function() {
+      code.open();
+      code.writeln(
+        html.value +
+          "<style>" +
+          css.value +
+          "</style>" +
+          "<script>" +
+          js.value +
+          "</script>"
+      );
+      code.close();
+    };
+  }
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="editor-interface">
+          <div className="instructions">
+            <h1>Instructions</h1>
+            <h2>Styliser un élément à l'aide d'une classe</h2>
+            <ul>
+              <li>Créer une balise div dans la section HTML</li>
+              <li></li>
+            </ul>
+          </div>
+          <div className="code-inputs">
+            <textarea id="html" placeholder="HTML"></textarea>
+            <textarea id="css" placeholder="CSS"></textarea>
+            <textarea id="js" placeholder="JavaScript"></textarea>
+          </div>
+        </div>
+        <div>
+          <iframe id="code"></iframe>
+        </div>
       </div>
     );
   }
